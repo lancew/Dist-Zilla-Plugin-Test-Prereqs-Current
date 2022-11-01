04d8aeb2fef452b41ca42c61d11894f6aee76005
Lance Wicks <lw@judocoach.com>
2022-11-01 22:16:19 +0000

    Add some pod, correct version in Changes file

--- lib/Dist/Zilla/Plugin/Test/Prereqs/Current.pm
+++ lib/Dist/Zilla/Plugin/Test/Prereqs/Current.pm
@@ -11,13 +11,51 @@ use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
 with 'Dist::Zilla::Role::FilePruner';
 
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Plugin::Test::Prereqs::Current - Dzil plugin to check prereqs are up to date
+
+=for HTML
+<a href=https://travis-ci.org/lancew/Webservice-Judobase><img src=https://api.travis-ci.org/lancew/Webservice-Judobase.svg?branch=master></a>
+<a href=https://coveralls.io/github/lancew/Webservice-Judobase><img src=https://coveralls.io/repos/lancew/Webservice-Judobase/badge.svg?branch=master></a>
+<a href=https://metacpan.org/pod/Webservice-Judobase><img src="https://badge.fury.io/pl/Webservice-Judobase.svg"></a>
+<a href=https://github.com/lancew/Webservice-Judobase/issues><img src=https://img.shields.io/github/issues/lancew/Webservice-Judobase.svg></a>
+
+=head1 DESCRIPTION
+
+Tests that the versions in the [Prereqs] setion of the dist.ini are up to date
+
+=head1 SYNOPSIS
+
+To be written.
+
+=head1 SUBLASSES / INTERFACES / ATTRIBUTES
+
+
 =head2 prune_files
 
 Stub for docs
 
+=head1 AUTHOR
+
+Lance Wicks <lancew@cpan.org>
+
+=head1 CONTRIBUTORS
+
+=head1 COPYRIGHT
+
+This software is Copyright (c) 2016-2021 by Lance Wicks.
+
+=head1 LICENSE
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
+See the LICENSE file in the distribution on CPAN for full text.
+
 =cut
-
-
 
 sub prune_files
 {
